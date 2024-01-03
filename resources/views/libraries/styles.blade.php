c49238111551adfa16fdf20c2e379ac39dfe416f
Nayomi-Samarapperuma <nayomi985571490@gmail.com>
2024-01-03 15:02:58 +0530

    test

---
+++ resources/views/libraries/styles.blade.php
@@ -0,0 +1,9 @@
+<link href="{{ asset('assets/css/nucleo-icons.css" rel="stylesheet') }}" />
+<link href="{{ asset('assets/css/nucleo-svg.css" rel="stylesheet') }}" />
+<link href="{{ asset('assets/css/nucleo-svg.css" rel="stylesheet') }}" />
+<link id="pagestyle" href="{{ asset('assets/css/argon-dashboard.min.css?v=2.0.5" rel="stylesheet') }}" />
+<style>
+    .async-hide {
+      opacity: 0 !important
+    }
+  </style>
