6f310595443eb75a5cd72851095782d9b66fea7d
Nayomi-Samarapperuma <nayomi985571490@gmail.com>
2024-01-04 20:30:35 +0530

    https://github.com/Nayomi-Samarapperuma/Template-Integration-laravel.git

--- resources/views/libraries/styles.blade.php
+++ resources/views/libraries/styles.blade.php
@@ -4,6 +4,6 @@
 <link id="pagestyle" href="{{ asset('assets/css/argon-dashboard.min.css?v=2.0.5" rel="stylesheet') }}" />
 <style>
     .async-hide {
-      opacity: 0 !important
+        opacity: 0 !important
     }
-  </style>
+</style>
